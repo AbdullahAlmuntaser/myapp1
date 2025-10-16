01823d2acbcdcf1d2228a319abec4724d0ef6a9a
almuntaserabdullah780 <almuntaserabdullah780@gmail.com>
2025-10-16 14:57:48 +0000

    nb

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,32 +1,15 @@
-{ pkgs, ... }:
-let
-  android_sdk_packages = pkgs.androidenv.composeAndroidPackages {
-    platform-tools = true;
-    build-tools = ["34.0.0"];
-    platforms = ["android-36"]; # Matched with compileSdk=36
-    cmdline-tools = ["latest"];
-  };
-in
-{
+{ pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
-
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.flutter
-    pkgs.dart-sdk # Explicitly include Dart SDK
+    pkgs.dart
     pkgs.jdk21
     pkgs.unzip
   ];
-
-  # Android SDK configuration
-  android_sdk = android_sdk_packages;
-
   # Sets environment variables in the workspace
-  env = {
-    ANDROID_HOME = "${android_sdk_packages}";
-  };
-
+  env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -34,14 +17,9 @@ in
       "Dart-Code.dart-code"
     ];
     workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        get-deps = "flutter pub get";
-      };
-      # Runs on every workspace start
-      onStart = {
-        doctor = "flutter doctor -v";
-      };
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = { };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
     };
     # Enable previews and customize configuration
     previews = {
