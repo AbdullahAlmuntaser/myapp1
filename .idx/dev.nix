a406f9c0302231b51e8ef09d70d99713007cd93f
almuntaserabdullah780 <almuntaserabdullah780@gmail.com>
2025-10-16 14:56:38 +0000

    vbcd

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,40 +1,61 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  android_sdk_packages = pkgs.androidenv.composeAndroidPackages {
+    platform-tools = true;
+    build-tools = ["34.0.0"];
+    platforms = ["android-36"]; # Matched with compileSdk=36
+    cmdline-tools = ["latest"];
+  };
+in
+{
   # Which nixpkgs channel to use.
-    channel = "stable-24.05"; # or "unstable"
-      # Use https://search.nixos.org/packages to find packages
-        packages = [
-            pkgs.jdk21
-                pkgs.unzip
-                  ];
-                    # Sets environment variables in the workspace
-                      env = {};
-                        idx = {
-                            # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-                                extensions = [
-                                      "Dart-Code.flutter"
-                                            "Dart-Code.dart-code"
-                                                ];
-                                                    workspace = {
-                                                          # Runs when a workspace is first created with this `dev.nix` file
-                                                                onCreate = { };
-                                                                      # To run something each time the workspace is (re)started, use the `onStart` hook
-                                                                          };
-                                                                              # Enable previews and customize configuration
-                                                                                  previews = {
-                                                                                        enable = true;
-                                                                                              previews = {
-                                                                                                      web = {
-                                                                                                                command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-                                                                                                                          manager = "flutter";
-                                                                                                                                  };
-                                                                                                                                          android = {
-                                                                                                                                                    command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
-                                                                                                                                                              manager = "flutter";
-                                                                                                                                                                      };
-                                                                                                                                                                            };
-                                                                                                                                                                                };
-                                                                                                                                                                                  };
-                                                                                                                                                                                  }
-                                                                                                                                                                                  
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.flutter
+    pkgs.dart-sdk # Explicitly include Dart SDK
+    pkgs.jdk21
+    pkgs.unzip
+  ];
+
+  # Android SDK configuration
+  android_sdk = android_sdk_packages;
+
+  # Sets environment variables in the workspace
+  env = {
+    ANDROID_HOME = "${android_sdk_packages}";
+  };
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+    ];
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        get-deps = "flutter pub get";
+      };
+      # Runs on every workspace start
+      onStart = {
+        doctor = "flutter doctor -v";
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
+        };
+        android = {
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
+        };
+      };
+    };
+  };
+}
